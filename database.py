ac07bf01e9af249f50694219db4df20b0b80927d
Teoh Yee Xian <yeexian_tyx@hotmail.com>
2024-02-23 20:48:54 +0800

    minor

--- database.py
+++ database.py
@@ -28,7 +28,7 @@ def load_csv():
 
     return mean_s, std_s, data_s
 
-@st.cache_data()
+# @st.cache_data()
 def load_data(symbol, period, today):
     time.sleep(2)
     sm = yf.download(symbol, period=period)
